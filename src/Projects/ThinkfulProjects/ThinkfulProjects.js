d7b326e7d0d46f16093b6e3e35075730e9c2d182
unknown <mrloganellis@gmail.com>
2020-04-08 22:33:34 -0400

    fixed project descriptions

--- src/Projects/ThinkfulProjects/ThinkfulProjects.js
+++ src/Projects/ThinkfulProjects/ThinkfulProjects.js
@@ -14,7 +14,7 @@ class ThinkfulProjects extends Component {
           <h2 className="projectTitle">Space Exploration</h2>
           <img src={SpaceExploration} className="projectImage" alt="Space Exploration Landing Page"/>
           <p className="projectDescription">
-          This application gives the user the ability to explore space without leaving their house! This site allows people to look up NASA’s picture of the day and accompanying YouTube videos, as well as lookup space search terms that come with pictures and accompanying youtube videos.  
+          This application gives the user the ability to explore space without leaving their house! This site allows people to look up NASA’s picture of the day and accompanying YouTube videos, as well as lookup space search terms that come with pictures and accompanying youtube videos. I made this because I think space is something that is super cool, and shouldn't be difficult to learn about.
           </p>
           <p className="projectLanguages">
             This application is a front-end application and was built using HTML5, CSS3, JavaScript ES6, jQuery, and RESTful APIs.
@@ -32,7 +32,7 @@ class ThinkfulProjects extends Component {
           <h2 className="projectTitle">Community Artists</h2>
             <img src={CommunityArtist} className="projectImage" alt="Community Artists Landing Page"/>
             <p className="projectDescription">
-            This application is a start to help bridge a gap between the art community and other communities. This application will allow you to upload images to a database and view said images.   
+            This application is a start to help bridge a gap between the art community and other communities. This application will allow you to upload images to a database and view said images. This is a great place for people who want to keep up with the art community or join the art community and don't know where to begin. I have had a mere glimpse into the art community, but I noticed that if someone doesn't know where to look then it's hard to figure it out. That's where this site comes in handy.
             </p>
             <p className="projectLanguages">
               This application is a full-stack application and was built using HTML5, CSS3, JavaScript ES6, React, Nodejs, Express, Postgresql, Jest, Mochai, Chai, and Supertest.
@@ -56,7 +56,7 @@ class ThinkfulProjects extends Component {
         <h2 className="projectTitle">Jobs R Us</h2>
             <img src={JobsRUs} className="projectImage" alt="Community Artists Landing Page"/>
             <p className="projectDescription">
-            This application is a job board for anyone to use. Most job boards these days are either too expensive or have a niche; this one is designed so that it's simple, cheap, and can be for any type of job!   
+            This application is a job board for anyone to use. Most job boards these days are either too expensive or have a niche; this one is designed so that it's simple, cheap, and can be for any type of job! Jobs R' Us is a great place for small businesses who don't want to pay high fees or expand their search by not being part if a specific niche. I built this app because one thing I wantt to be able to do is help small businesses grow with the age of technology, and not get left behind. 
             </p>
             <p className="projectLanguages">
               This application is a full-stack application and was built using HTML5, CSS3, JavaScript ES6, React, Nodejs, Express, Postgresql, Jest, Mochai, Chai, and Supertest.
